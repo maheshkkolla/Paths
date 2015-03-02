f9d0a82f17cb447b39444ff296eb7c2a8e40746f
unknown <maheshkk@INMAHESKK.corporate.thoughtworks.com>
2015-03-02 10:28:16 +0530

    version 1 gives true if direct path is exists gives flase if no direct path prints no city in db if city is not present s Please enter the commit message for your changes. Lines starting

---
+++ Paths.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+class City {
+	List<String> directTo = new ArrayList<String>();
+	String name;
+	public City(String name){
+		this.name = name;
+	}
+	public void addDirectPathTo(String to) {
+		directTo.add(to);
+	}
+}
+
+public class Paths  {
+	Map<String, City> allCities = new Hashtable<String, City>();
+
+	public Paths(String[] cities) {
+		for (String city: cities) {
+			allCities.put(city, new City(city));
+		}
+	}
+
+	public void addDirectPath(String from, String to) {
+		allCities.get(from).addDirectPathTo(to);
+		allCities.get(to).addDirectPathTo(from);
+	}
+
+	public Boolean hasDirectPath(String from, String to){
+		City fromCity = allCities.get(from);
+		return fromCity.directTo.contains(to); 
+	}
+
+	public Boolean isCity(String city) {
+		if(allCities.get(city) != null) return true;
+		return false;
+	}
+	public void addDirectPaths(String[][] directPaths) {
+		for (String[] directPath: directPaths ) {
+			this.addDirectPath(directPath[0], directPath[1]);
+		}
+	}
+	public Boolean areCities(String city1, String city2) {
+		Boolean city1Present = isCity(city1);
+		Boolean city2Present = isCity(city2);  
+		if(!city1Present) System.out.println("No City names \""+city1+"\" in Database");
+		if(!city2Present) System.out.println("No City names \""+city2+"\" in Database");
+		return(city1Present && city2Present); 
+	}
+	public static void  main(String[] args) {
+		String[] cities = {"Banglore","Singapore","Seoul","Beijing","Tokyo","Dubai"};
+		Paths paths = new Paths(cities);
+		String[][] directPaths = new String[][] { {"Banglore", "Singapore"}, {"Singapore","Seoul"},
+			{"Singapore","Dubai"}, {"Seoul","Beijing"}, {"Beijing","Tokyo"}
+		};
+		paths.addDirectPaths(directPaths);
+		if(paths.areCities(args[0], args[1])) {
+			Boolean a = paths.hasDirectPath(args[0], args[1]);
+			System.out.println(a);
+		}	
+	}	
+}
+
---
+++ PathsTest.java
@@ -0,0 +1,54 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.util.*;
+
+
+public class PathsTest {
+	@Test
+	public void isCity_returns_true_if_Banglore_present_in_the_database(){
+		String[] cities = {"Banglore","Singapore"};
+		Paths paths = new Paths(cities);
+		Boolean cityPresent = paths.isCity("Banglore");
+		assertEquals(cityPresent, true);
+	}
+	@Test
+	public void isCity_returns_false_if_Chennai_present_in_the_database(){
+		String[] cities = {"Banglore","Singapore"};
+		Paths paths = new Paths(cities);
+		Boolean cityPresent = paths.isCity("Chennai");
+		assertEquals(cityPresent, false);
+	}
+	@Test
+	public void addDirectPath_adds_direct_path_between_Banglore_and_Singapore() {
+		String[] cities = {"Banglore","Singapore"};
+		Paths paths = new Paths(cities);
+		paths.addDirectPath("Banglore","Singapore");
+		Boolean has = paths.hasDirectPath("Banglore","Singapore");
+		assertEquals(has, true);
+	}
+	@Test
+	public void hasDirectPath_returns_true_for_Banglore_and_Singapore() {
+		String[] cities = {"Banglore","Singapore"};
+		Paths paths = new Paths(cities);
+		paths.addDirectPath("Banglore","Singapore");
+		Boolean has = paths.hasDirectPath("Banglore","Singapore");
+		assertEquals(has, true);
+	}
+	@Test
+	public void hasDirectPath_returns_false_for_Banglore_and_Tokyo() {
+		String[] cities = {"Banglore","Singapore","Tokyo"};
+		Paths paths = new Paths(cities);
+		paths.addDirectPath("Banglore","Singapore");
+		Boolean has = paths.hasDirectPath("Banglore","Tokyo");
+		assertEquals(has, false);
+	}
+	@Test
+	public void hasDirectPath_returns_true_for_Singapore_and_Banglore_reverese_also() {
+		String[] cities = {"Banglore","Singapore"};
+		Paths paths = new Paths(cities);
+		paths.addDirectPath("Banglore","Singapore");
+		Boolean has = paths.hasDirectPath("Singapore","Banglore");
+		assertEquals(has, true);
+	}
+
+}
